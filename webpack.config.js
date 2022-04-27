7382168745caccea2bd9a3831307296cd8dff982
ARANY <v.aderibigbe96@gmail.com>
2022-04-27 00:45:18 -0400

    add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+        template: './src/index.html'
     }),
   ],
   output: {
